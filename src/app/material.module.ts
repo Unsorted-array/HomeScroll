2ba556ff71f24176850abed9ef7ca015ccf26c75
usorted-array <ricksrv.5@gmail.com>
2018-03-15 23:44:11 +0530

    added topBar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material';
+import {MatIconModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [MatButtonModule , MatMenuModule, MatToolbarModule , MatIconModule],
+  exports: [MatButtonModule , MatMenuModule , MatToolbarModule , MatIconModule],
 })
 export class MaterialModule { }
